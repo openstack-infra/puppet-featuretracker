5336c6aecc0917f1c8564b722a7aff21ad36374c
Jeremy Stanley <fungi@yuggoth.org>
2016-07-18 20:44:07 +0000

    Initial boilerplate files
    Basic module framework as output by cookiecutter using the
    openstack-infra/puppet-infra-cookiecutter template.
    
    Change-Id: Ibbbc6ba09cf7ce0253b1b8b3a3d6f511bcfd4516

---
+++ manifests/init.pp
@@ -0,0 +1,10 @@
+# == Class: userstory_dashboard
+#
+# Full description of class userstory_dashboard here.
+#
+# === Parameters
+#
+# [*sample_parameter*]
+#   Explanation of what this parameter affects and what it defaults to.
+#
+class userstory_dashboard {}
